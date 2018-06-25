ba2690a78a75da07ccb64cfb627a954245d2aaac
Jose Luis Diaz (pep) <jldiaz@uniovi.es>
2018-06-25 10:18:52 +0200

    Reduce startup time
    The startup time of the container was high because the image didn't contain the app.
    The app was downloaded and built during startup.
    
    I moved those stes to the Dockerfile. The advantage is faster startup. The downside
    is that a new image has to be built if the app source changes, but this is considered
    infrequent.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM aslushnikov/latex-online
 
+RUN rm -rf /var/www && git clone https://github.com/jldiaz/latex-online /var/www
+RUN cd /var/www && npm install .
+
 COPY ./util/docker-entrypoint.sh /
+
 
 EXPOSE 2700
 CMD ["./docker-entrypoint.sh"]
--- util/docker-entrypoint-orig.sh
+++ util/docker-entrypoint-orig.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+# Cleanup www folder
+rm -rf /var/www
+# Copy and install the latest & greatest Latex-Online
+git clone https://github.com/jldiaz/latex-online /var/www
 cd /var/www
+npm install .
+
 export NODE_ENV=production
 export VERSION=$(git rev-parse HEAD)
 
